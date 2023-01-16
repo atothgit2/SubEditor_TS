cd87acf79f5986e35182fddf22dac38b6a1dc087
Toth Arpad <arpifiokja@gmail.com>
2023-01-16 23:35:53 +0100

    service layer refactor starting

--- src/controller.ts
+++ src/controller.ts
@@ -1,39 +1,43 @@
 import { Request, Response, NextFunction } from "express";
-import { renameFile, createFile } from "../utils/utils";
 import fs from "fs";
 import path from "path";
+import { getFileList } from "../../SERVICE/src/service"
 
 export interface MetaData {
   title: String;
   imdbscore: Number;
 }
 
-// CREATE
-const uploadFile = async (req: Request, res: Response, next: NextFunction) => {
-  let file = req.file;
-  let metadata = JSON.stringify(req.body);
+// // CREATE
+// const uploadFile = async (req: Request, res: Response, next: NextFunction) => {
+//   let file = req.file;
+//   let metadata = JSON.stringify(req.body);
 
-  console.log(file);
-  console.log(metadata);
+//   console.log(file);
+//   console.log(metadata);
 
-  renameFile(
-    `uploads/${req.file?.filename}`,
-    `uploads/${req.file?.filename}.srt`
-  );
-  createFile(`uploads/${req.file?.filename}.json`, `${metadata}`);
+//   renameFile(
+//     `uploads/${req.file?.filename}`,
+//     `uploads/${req.file?.filename}.srt`
+//   );
+//   createFile(`uploads/${req.file?.filename}.json`, `${metadata}`);
 
-  res.status(200).send(`Succesfull upload!`);
-};
+//   res.status(200).send(`Succesfull upload!`);
+// };
 
 // GET ALL
-const getFileList = async (req: Request, res: Response, next: NextFunction) => {
-  fs.readdir(path.join(__dirname, "..", "..", "uploads"), (err, data) => {
-    if (err) throw err;
-    res.status(200).send(data);
-  });
+const listFiles = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+      let data = getFileList()
+      res.status(200).send(data);
+    
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // GET ONE W/ CONTENT
+// TODO: if I send wrong id in the url, the complete server dies and need to restart
 const getFileContent = async (req: Request,res: Response, next: NextFunction) => {
   let id: string = req.params.id;
 
@@ -89,8 +93,7 @@ const deleteFile = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 export default {
-  uploadFile,
-  getFileList,
+  listFiles,
   getFileContent,
   editFileContent,
   editMetaData,
--- src/routes.ts
+++ src/routes.ts
@@ -5,8 +5,8 @@ import multer from "multer";
 const router = express.Router();
 const upload = multer({ dest: "./uploads" });
 
-router.post("/upload", upload.single("file"), controller.uploadFile);
-router.get("/file", controller.getFileList);
+router.post("/upload", upload.single("file"), controller.listFiles);
+router.get("/file", controller.listFiles);
 router.get("/file/:id", controller.getFileContent);
 router.put("/file/:id", controller.editFileContent);
 router.put("/file/meta/:id", upload.none(), controller.editMetaData);
